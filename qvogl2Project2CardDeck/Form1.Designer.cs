4763ad5bb358b087b4bb7cb6f44cc4fcdf8bdbf0
qvogl2 <qvogl2@NUTVDIWIN10-03>
2020-01-21 14:49:38 -0600

    Add project files.

---
+++ qvogl2Project2CardDeck/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace qvogl2Project2CardDeck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawAmount = new System.Windows.Forms.TextBox();
+            this.cardPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawAmount
+            // 
+            this.drawAmount.Location = new System.Drawing.Point(47, 10);
+            this.drawAmount.Name = "drawAmount";
+            this.drawAmount.Size = new System.Drawing.Size(37, 20);
+            this.drawAmount.TabIndex = 1;
+            // 
+            // cardPictureBox
+            // 
+            this.cardPictureBox.Image = global::qvogl2Project2CardDeck.Properties.Resources.cardBack;
+            this.cardPictureBox.Location = new System.Drawing.Point(12, 45);
+            this.cardPictureBox.Name = "cardPictureBox";
+            this.cardPictureBox.Size = new System.Drawing.Size(72, 100);
+            this.cardPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardPictureBox.TabIndex = 2;
+            this.cardPictureBox.TabStop = false;
+            this.cardPictureBox.Click += new System.EventHandler(this.cardPictureBox_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Draw:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 405);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cardPictureBox);
+            this.Controls.Add(this.drawAmount);
+            this.Name = "Form1";
+            this.Text = "Card Deck";
+            ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox drawAmount;
+        private System.Windows.Forms.PictureBox cardPictureBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ qvogl2Project2CardDeck/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qvogl2Project2CardDeck
+{
+    public partial class Form1 : Form
+    {
+
+        List<String> Cards = new List<string>(new String[] { "2c", "2d", "2s", "2h",
+                                                                "3c", "3d", "3s", "3h",
+                                                                "4c", "4d", "4s", "4h",
+                                                                "5c", "5d", "5s", "5h",
+                                                                "6c", "6d", "6s", "6h",
+                                                                "7c", "7d", "7s", "7h",
+                                                                "8c", "8d", "8s", "8h",
+                                                                "9c", "9d", "9s", "9h",
+                                                                "10c", "10d", "10s", "10h",
+                                                                "jc", "jd", "js", "jh",
+                                                                "qc", "qd", "qs", "qh",
+                                                                "kc", "kd", "ks", "kh",
+                                                                "ac", "ad", "as", "ah" });
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cardPictureBox_Click(object sender, EventArgs e)
+        {
+
+            SoundPlayer shuffleSound = new SoundPlayer(Properties.Resources.shuffle);
+            shuffleSound.Play();
+
+            foreach (Control child in Controls)
+            {
+                if (child is PictureBox && child.Name.Contains("Card"))
+                    ((PictureBox)child).Dispose();               
+            }
+            List<String> drawnCards = new List<String>();
+
+            int numOfCards = 0;
+            try
+            {
+                numOfCards = Convert.ToInt32(drawAmount.Text);
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Please input 0-52 cards.");
+            }
+
+            if (numOfCards > 52 || numOfCards < 0)
+            {
+                MessageBox.Show("Invalid entry. Please select 0-52 cards.");
+                return;
+            }
+            
+            int drawn = 0;
+            Random random = new Random();
+            String cardSelected;
+            int x = 0;
+            int y = 110;
+            while (drawn < numOfCards)
+            {
+                PictureBox pictureBox1 = new PictureBox();
+
+                if (drawn % 13 == 0)
+                {
+                    y += 45;
+                    x = 12;
+                }
+                drawn++;
+                
+
+                cardSelected = Cards[random.Next(0, Cards.Count)];
+                while (drawnCards.Contains(cardSelected))
+                {
+                    cardSelected = Cards[random.Next(0, Cards.Count)];
+                }
+                drawnCards.Add(cardSelected);
+                //Console.WriteLine(cardSelected);
+                
+
+                switch (cardSelected)
+                {
+                    case "2c": pictureBox1.Image = Properties.Resources._2_of_clubs; break;
+                    case "2d": pictureBox1.Image = Properties.Resources._2_of_diamonds; break;
+                    case "2s": pictureBox1.Image = Properties.Resources._2_of_spades; break;
+                    case "2h": pictureBox1.Image = Properties.Resources._2_of_hearts; break;
+                    case "3c": pictureBox1.Image = Properties.Resources._3_of_clubs; break;
+                    case "3d": pictureBox1.Image = Properties.Resources._3_of_diamonds; break;
+                    case "3s": pictureBox1.Image = Properties.Resources._3_of_spades; break;
+                    case "3h": pictureBox1.Image = Properties.Resources._3_of_hearts; break;
+                    case "4c": pictureBox1.Image = Properties.Resources._4_of_clubs; break;
+                    case "4d": pictureBox1.Image = Properties.Resources._4_of_diamonds; break;
+                    case "4s": pictureBox1.Image = Properties.Resources._4_of_spades; break;
+                    case "4h": pictureBox1.Image = Properties.Resources._4_of_hearts; break;
+                    case "5c": pictureBox1.Image = Properties.Resources._5_of_clubs; break;
+                    case "5d": pictureBox1.Image = Properties.Resources._5_of_diamonds; break;
+                    case "5s": pictureBox1.Image = Properties.Resources._5_of_spades; break;
+                    case "5h": pictureBox1.Image = Properties.Resources._5_of_hearts; break;
+                    case "6c": pictureBox1.Image = Properties.Resources._6_of_clubs; break;
+                    case "6d": pictureBox1.Image = Properties.Resources._6_of_diamonds; break;
+                    case "6s": pictureBox1.Image = Properties.Resources._6_of_spades; break;
+                    case "6h": pictureBox1.Image = Properties.Resources._6_of_hearts; break;
+                    case "7c": pictureBox1.Image = Properties.Resources._7_of_clubs; break;
+                    case "7d": pictureBox1.Image = Properties.Resources._7_of_diamonds; break;
+                    case "7s": pictureBox1.Image = Properties.Resources._7_of_spades; break;
+                    case "7h": pictureBox1.Image = Properties.Resources._7_of_hearts; break;
+                    case "8c": pictureBox1.Image = Properties.Resources._8_of_clubs; break;
+                    case "8d": pictureBox1.Image = Properties.Resources._8_of_diamonds; break;
+                    case "8s": pictureBox1.Image = Properties.Resources._8_of_spades; break;
+                    case "8h": pictureBox1.Image = Properties.Resources._8_of_hearts; break;
+                    case "9c": pictureBox1.Image = Properties.Resources._9_of_clubs; break;
+                    case "9d": pictureBox1.Image = Properties.Resources._9_of_diamonds; break;
+                    case "9s": pictureBox1.Image = Properties.Resources._9_of_spades; break;
+                    case "9h": pictureBox1.Image = Properties.Resources._9_of_hearts; break;
+                    case "10c": pictureBox1.Image = Properties.Resources._10_of_clubs; break;
+                    case "10d": pictureBox1.Image = Properties.Resources._10_of_diamonds; break;
+                    case "10s": pictureBox1.Image = Properties.Resources._10_of_spades; break;
+                    case "10h": pictureBox1.Image = Properties.Resources._10_of_hearts; break;
+                    case "jc": pictureBox1.Image = Properties.Resources.jack_of_clubs2; break;
+                    case "jd": pictureBox1.Image = Properties.Resources.jack_of_diamonds2; break;
+                    case "js": pictureBox1.Image = Properties.Resources.jack_of_spades2; break;
+                    case "jh": pictureBox1.Image = Properties.Resources.jack_of_hearts2; break;
+                    case "qc": pictureBox1.Image = Properties.Resources.queen_of_clubs2; break;
+                    case "qd": pictureBox1.Image = Properties.Resources.queen_of_diamonds2; break;
+                    case "qs": pictureBox1.Image = Properties.Resources.queen_of_spades2; break;
+                    case "qh": pictureBox1.Image = Properties.Resources.queen_of_hearts2; break;
+                    case "kc": pictureBox1.Image = Properties.Resources.king_of_clubs2; break;
+                    case "kd": pictureBox1.Image = Properties.Resources.king_of_diamonds2; break;
+                    case "ks": pictureBox1.Image = Properties.Resources.king_of_spades2; break;
+                    case "kh": pictureBox1.Image = Properties.Resources.king_of_hearts2; break;
+                    case "ac": pictureBox1.Image = Properties.Resources.ace_of_clubs; break;
+                    case "ad": pictureBox1.Image = Properties.Resources.ace_of_diamonds; break;
+                    case "as": pictureBox1.Image = Properties.Resources.ace_of_spades; break;
+                    case "ah": pictureBox1.Image = Properties.Resources.ace_of_hearts; break;
+                    
+                }
+
+                pictureBox1.Name = "Card" + drawn;
+                pictureBox1.Size = new Size(72, 100);
+                pictureBox1.Location = new Point(x, y);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                this.Controls.Add(pictureBox1);
+
+                pictureBox1.BringToFront();
+
+                x += 12;
+
+            }
+
+        }
+
+    }
+}
